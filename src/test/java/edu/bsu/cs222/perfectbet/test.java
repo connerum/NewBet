4d1250dcab323c1270a0eeafe04ac1d8b4a63211
davisknight7 <davis.knight@bsu.edu>
2022-10-19 10:57:08 -0400

    Added Test Resource Folder and test.json

--- src/test/java/edu/bsu/cs222/perfectbet/test.java
+++ src/test/java/edu/bsu/cs222/perfectbet/test.java
@@ -2,7 +2,8 @@ package edu.bsu.cs222.perfectbet;
 
 public class test {
     public void tester() {
-        //InputStream sampleFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");x
+        // Code to get test.json stream:
+        // InputStream sampleFileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");x
 
     }
 }
