53261376e164db7f4a16f5eaab74ad65ec855448
evyncking <ecking@bsu.edu>
2022-10-21 10:23:48 -0400

    Added SportsNameFormatter class

---
+++ src/main/java/edu/bsu/cs222/perfectbet/SportsNameFormatter.java
@@ -0,0 +1,22 @@
+package edu.bsu.cs222.perfectbet;
+
+public class SportsNameFormatter {
+    //Reformats the selected sport name to be able to work through the API url and returns as String
+    public String NameReformatted(String userChoice) {
+        if (userChoice.contains("American Football")) {
+            return "americanfootball";
+        }
+        else if (userChoice.contains("Baseball")){
+            return "baseball_mlb";
+        }
+        else if (userChoice.contains("Ice Hockey")) {
+            return "icehockey";
+        }
+        else if (userChoice.contains("Mixed Martial Arts")) {
+            return "mma_mixed_martial_arts";
+        }
+        else {
+            return userChoice;
+        }
+    }
+}
