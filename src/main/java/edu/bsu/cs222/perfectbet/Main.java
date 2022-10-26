7a87b41f9b6bbfc789c3f2c8bcd85fc2729af1ac
connerum <98507011+connerum@users.noreply.github.com>
2022-10-26 10:13:48 -0400

    Added Icon to Stage & GUI

--- src/main/java/edu/bsu/cs222/perfectbet/Main.java
+++ src/main/java/edu/bsu/cs222/perfectbet/Main.java
@@ -3,6 +3,7 @@ package edu.bsu.cs222.perfectbet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainGui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("coin.png")));
         stage.setTitle("The Perfect Bet");
         stage.setScene(scene);
         stage.show();
